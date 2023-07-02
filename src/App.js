7e6d724063c8fe44373babcb8d85fa669aedd437
Sanmitha <sanmithasadanand@gmail.com>
2023-07-03 03:04:44 +0530

    Create login and registration pages

--- src/App.js
+++ src/App.js
@@ -4,15 +4,22 @@ import About from './Components/About';
 import Testimonial from './Components/Testimonial';
 import Contact from './Components/Contact';
 import Footer from './Components/Footer';
+import { Login } from './Components/Login';
+import { Register } from './Components/Register';
+import { useState } from 'react';
 
 function App() {
+  const [currentForm, setCurrentForm] = useState('login');
+
+  const toggleForm = (formName) => {
+    setCurrentForm(formName);
+  }
+
   return (
     <div className="App">
-      <Home />
-      <About />
-      <Testimonial />
-      <Contact />
-      <Footer />
+      {
+        currentForm === "login" ? <Login onFormSwitch={toggleForm} /> : <Register onFormSwitch={toggleForm} />
+      }
     </div>
   );
 }
