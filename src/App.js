d9b26730603ce928b03f5f7c4b7c1d660300e531
Sanmitha <sanmithasadanand@gmail.com>
2023-07-03 01:56:56 +0530

    Add contact us section

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import Home from './Components/Home';
 import About from './Components/About';
 import Testimonial from './Components/Testimonial';
+import Contact from './Components/Contact';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Home />
       <About />
       <Testimonial />
+      <Contact />
     </div>
   );
 }
